609ae99378f6579c53afe5b176b674a78bb3d1ac
Xiaoting Chen <eileenchen1949@gmail.com>
2022-10-03 17:49:12 -0400

    plots

--- buffer_test.R
+++ buffer_test.R
@@ -7,16 +7,15 @@ buffer <- data.frame(buffer = c(0.0001,0.0002,0.00025,0.0003,0.00035,0.0004,0.00
 x<-buffer$buffer
 y<-buffer$proportion
 plot(x,y, col=ifelse(x %in% c(0.00045,0.0012), 'red', 'black'),
-     xlab="Coordinates threshold", ylab="Outreach proportion",
-     cex.axis = 1.3, cex.lab = 1.3)
-lines(x,y)
-text(x[7]+0.00035, y[7]+0.0002, labels=x[7])
-text(x[15]+0.0003, y[15]+0.0004, labels=x[15])
+     xlab="Coordinates threshold [degrees]", ylab="Outreach proportion",
+     cex.axis = 1.5, cex.lab = 1.8, cex = 2)
+lines(x,y, lwd = 2)
+text(x[7]+0.0004, y[7]+0.00025, labels=x[7],cex= 1.5)
+text(x[15]+0.00035, y[15]+0.00055, labels=x[15],cex= 1.5)
 
 
 line <- data.frame(buffer =  seq(0.001,0.006,0.0005),
                    proportion=c(0.864,0.855,0.845,0.834,0.822,0.81,0.798,0.786,0.773,0.761,0.748))
 lmod <- lm(proportion~buffer,data = line)
 predict(lmod,newx=0.0009)
-
 
